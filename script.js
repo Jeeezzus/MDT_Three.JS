dee0b8bfbee3aeb9981fef2ca0e833c671458321
Gauthier Gendreau <gauthier.gendreau@outlook.com>
2024-01-09 10:26:15 +0100

    added freezer models

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const renderer = new THREE.WebGLRenderer({
     // Antialiasing is used to smooth the edges of what is rendered
     antialias: true,
     // Activate the support of transparency
-    alpha: true
+    alpha: false
 });
 
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -41,26 +41,17 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(window.devicePixelRatio);
 });
 
-
 // Instantiate the loader
 const computer = new GLTFLoader();
 
 // Load a GLTF resource
 computer.load(
   // URL to the GLTF resource
-  'models/color/color.glb',
+  'BaseV2.gltf',
   
   // Called when the resource is loaded
   function ( gltf ) {
-    gltf.scene.traverse(function (node) {
-        if (node.isMesh) {
-            node.castShadow = true;
-            node.receiveShadow = true;
-        }
-    });
     scene.add( gltf.scene );
-    gltf.scene.scale.set(10, 10, 10)
-    gltf.scene.rotation.y = Math.PI/16;
 
     // You might want to call the render or animate function here
   },
@@ -76,161 +67,13 @@ computer.load(
   }
 );
 
-// Instantiate the loader
-const chair = new GLTFLoader();
-
-// Load a GLTF resource
-chair.load(
-  // URL to the GLTF resource
-  'models/antique_wooden_chair.glb',
-  
-  // Called when the resource is loaded
-  function ( gltf ) {
-    gltf.scene.traverse(function (node) {
-        if (node.isMesh) {
-            node.castShadow = true;
-            node.receiveShadow = true;
-        }
-    });
-    scene.add( gltf.scene );
-    gltf.scene.scale.set(6, 6, 6)
-    gltf.scene.position.set(-5,-4.5,3)
-    gltf.scene.rotation.y = Math.PI / 2;
-
-    // You might want to call the render or animate function here
-  },
-
-  // Called while loading is progressing
-  function ( xhr ) {
-    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-  },
-
-  // Called when loading has errors
-  function ( error ) {
-    console.log( 'An error happened' );
-  }
-);
-
-// Instantiate the loader
-const desk = new GLTFLoader();
-desk.load(
-    // URL to the GLTF resource
-    'models/victorian_style_tabledesk.glb',
-    
-    // Called when the resource is loaded
-    function ( gltf ) {
-        gltf.scene.traverse(function (node) {
-            if (node.isMesh) {
-                node.castShadow = true;
-                node.receiveShadow = true;
-            }
-        });
-      scene.add( gltf.scene );
-      gltf.scene.scale.set(0.02, 0.02, 0.02)
-      gltf.scene.position.set(-1,1,0)
-      gltf.scene.rotation.y = Math.PI;
-  
-      // You might want to call the render or animate function here
-    },
-  
-    // Called while loading is progressing
-    function ( xhr ) {
-      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    },
-  
-    // Called when loading has errors
-    function ( error ) {
-      console.log( 'An error happened' );
-    }
-  );
-
-  // Instantiate the loader
-const lamp = new GLTFLoader();
-
-// Load a GLTF resource
-lamp.load(
-  // URL to the GLTF resource
-  'models/lamp_draft.glb',
-  
-  // Called when the resource is loaded
-  function ( gltf ) {
-    gltf.scene.traverse(function (node) {
-        if (node.isMesh) {
-            node.castShadow = true;
-            node.receiveShadow = true;
-        }
-    });
-    scene.add( gltf.scene );
-    gltf.scene.scale.set(0.5, 0.5, 0.5)
-    gltf.scene.position.set(0.5,1.5,-4)
-    gltf.scene.rotation.y = -31 * Math.PI/30;
-
-    // You might want to call the render or animate function here
-  },
-
-  // Called while loading is progressing
-  function ( xhr ) {
-    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-  },
-
-  // Called when loading has errors
-  function ( error ) {
-    console.log( 'An error happened' );
-  }
-);
-
-const planeGeometry = new THREE.PlaneGeometry(500, 500);
-const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x808080 });
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = -Math.PI / 2;
-plane.position.y = -4.5;
-plane.receiveShadow = true;
-scene.add(plane);
-
-const fb = new FBXLoader();
-let mixer; // For the animation
-
-fb.load('models/Sitting.fbx', (fbx) => {
-    // Add the loaded object to the scene
-    fbx.traverse(function (node) {
-        if (node.isMesh) {
-            node.castShadow = true;
-            node.receiveShadow = true;
-        }
-    });
-    scene.add(fbx);
-    fbx.scale.set(0.07, 0.07, 0.07)
-    fbx.position.set(-6.2,-4.8,0.8)
-    fbx.rotation.y = Math.PI / 2;
-    fbx.castShadow = true;
-
-    // If the FBX file has animations
-    if (fbx.animations && fbx.animations.length > 0) {
-        mixer = new THREE.AnimationMixer(fbx);
-        fbx.animations.forEach((clip) => {
-            mixer.clipAction(clip).play();
-        });
-    }
-}, undefined, (error) => {
-    console.error(error);
-});
-
-
-
-const ambientLight = new THREE.AmbientLight(0x9999ff, 0.5); // soft white light
+const ambientLight = new THREE.AmbientLight(0x9999ff, 0.8); // soft white light
 scene.add(ambientLight);
-
-const pointLight = new THREE.PointLight(0xfffaf0, 350, 300);
-pointLight.position.set(0.5,5,-4);
-pointLight.castShadow = true;
-scene.add(pointLight);
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(5, 10, 7.5);
 light.castShadow = true;
 scene.add(light);
-
-const clock = new THREE.Clock();
 // Create the animation loop
 const animate = () => {
     // Call animate recursively
@@ -238,8 +81,6 @@ const animate = () => {
 
     // Update the controls
     controls.update();
-    const delta = clock.getDelta();
-    if (mixer) mixer.update(delta);
 
     // Render the scene
     renderer.render(scene, camera);
