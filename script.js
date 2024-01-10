07382cee41f1e680a16914f716fff4a6f0d27606
Gauthier Gendreau <gauthier.gendreau@outlook.com>
2024-01-10 11:11:49 +0100

    vent displacement, auto orbit, and smooth animation

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const renderer = new THREE.WebGLRenderer({
     // Antialiasing is used to smooth the edges of what is rendered
     antialias: true,
     // Activate the support of transparency
-    alpha: false
+    alpha: true
 });
 
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -59,44 +59,105 @@ window.addEventListener('click', onMouseClick);
 const targetPosition = new THREE.Vector3(10, 10, 10); // Change to your desired position
 const moveDistance = 5; // The distance to move the object
 
+let autoRotateSpeed = 0.1; // Speed of auto-rotation
+let isAutoRotating = true; // Flag to enable/disable auto-rotation
 
+function updateAutoOrbit() {
+    if (isAutoRotating) {
+        controls.update();
+        controls.autoRotate = true;
+        controls.autoRotateSpeed = autoRotateSpeed;
+    }
+}
+
+controls.addEventListener('start', () => {
+  isAutoRotating = false;
+  controls.autoRotate = false;
+});
+
+let autoRotateTimeout;
+
+function resetAutoRotate() {
+    clearTimeout(autoRotateTimeout);
+    autoRotateTimeout = setTimeout(() => {
+        isAutoRotating = true;
+    }, 5000); // 5 seconds of inactivity
+}
+
+controls.addEventListener('end', resetAutoRotate);
+
+
+let ventmoved = false;
 function onObjectSelected(object) {
     console.log("Selected object name:", object.name);
     if(object.name == "corps" & object.moved == false){
-      object.position.copy(new THREE.Vector3(object.position.x, object.position.y - 20, object.position.z));
+      startAnimation(object, object.position.clone(),500,new THREE.Vector3(object.position.x, object.position.y - 20, object.position.z));
       object.moved = true;
     }else if(object.name == "foot" & object.moved == false){
-      object.position.copy(new THREE.Vector3(object.position.x, object.position.y - 30, object.position.z));
+      startAnimation(object, object.position.clone(),500,new THREE.Vector3(object.position.x, object.position.y - 30, object.position.z));
       object.moved = true;
     }else if(object.name == "gcloche" & object.moved == false){
-      object.position.copy(new THREE.Vector3(object.position.x, object.position.y + 40, object.position.z));
+      startAnimation(object, object.position.clone(),500,new THREE.Vector3(object.position.x, object.position.y + 40, object.position.z));
       object.moved = true;
     }else if(object.name == "pcloche" & object.moved == false){
-      object.position.copy(new THREE.Vector3(object.position.x, object.position.y + 25, object.position.z));
+      startAnimation(object, object.position.clone(),500,new THREE.Vector3(object.position.x, object.position.y + 25, object.position.z));
       object.moved = true;
     }else if(object.name == "silcring" & object.moved == false){
-      object.position.copy(new THREE.Vector3(object.position.x, object.position.y + 7, object.position.z));
+      startAnimation(object, object.position.clone(),500,new THREE.Vector3(object.position.x, object.position.y + 7, object.position.z));
       object.moved = true;
     }else if(object.name == "top" & object.moved == false){
-      object.position.copy(new THREE.Vector3(object.position.x, object.position.y + 9, object.position.z));
+      startAnimation(object, object.position.clone(),500,new THREE.Vector3(object.position.x, object.position.y + 9, object.position.z));
       object.moved = true;
     }else if(object.name == "ice" & object.moved == false){
-      object.position.copy(new THREE.Vector3(object.position.x, object.position.y + 12, object.position.z));
+      startAnimation(object, object.position.clone(),500,new THREE.Vector3(object.position.x, object.position.y + 12, object.position.z));
       object.moved = true;
-    }else if(object.name == "v4" & object.moved == false){
-      const rootObject = gltfRootObjects['v4']; // Access the stored root object
-
-      if (rootObject) {
-        rootObject.position.copy(new THREE.Vector3(
-            rootObject.position.x, 
-            rootObject.position.y, 
-            rootObject.position.z + 5
+    }else if(object.name.includes("v") & ventmoved == false){
+      startAnimation(gltfRootObjects['v2'], gltfRootObjects['v2'].position.clone(),500,new THREE.Vector3(
+        gltfRootObjects['v2'].position.x - 10, 
+        gltfRootObjects['v2'].position.y, 
+        gltfRootObjects['v2'].position.z
         ));
-        }
-      object.moved = true;
+        startAnimation(gltfRootObjects['v4'], gltfRootObjects['v4'].position.clone(),500,new THREE.Vector3(
+          gltfRootObjects['v4'].position.x, 
+          gltfRootObjects['v4'].position.y, 
+          gltfRootObjects['v4'].position.z + 10
+        ));
+        startAnimation(gltfRootObjects['v6'], gltfRootObjects['v6'].position.clone(),500,new THREE.Vector3(
+          gltfRootObjects['v6'].position.x + 10, 
+          gltfRootObjects['v6'].position.y, 
+          gltfRootObjects['v6'].position.z
+        ));
+        startAnimation(gltfRootObjects['v8'], gltfRootObjects['v8'].position.clone(),500,new THREE.Vector3(
+          gltfRootObjects['v8'].position.x, 
+          gltfRootObjects['v8'].position.y, 
+          gltfRootObjects['v8'].position.z - 10
+        ));
+        startAnimation(gltfRootObjects['v1'], gltfRootObjects['v1'].position.clone(),500,new THREE.Vector3(
+          gltfRootObjects['v1'].position.x - (0.70710678118*10), 
+          gltfRootObjects['v1'].position.y, 
+          gltfRootObjects['v1'].position.z - (0.70710678118*10)
+        ));
+        startAnimation(gltfRootObjects['v5'], gltfRootObjects['v5'].position.clone(),500,new THREE.Vector3(
+          gltfRootObjects['v5'].position.x + (0.70710678118*10), 
+          gltfRootObjects['v5'].position.y, 
+          gltfRootObjects['v5'].position.z + (0.70710678118*10)
+        ));
+        startAnimation(gltfRootObjects['v7'], gltfRootObjects['v7'].position.clone(),500,new THREE.Vector3(
+          gltfRootObjects['v7'].position.x + (0.70710678118*10), 
+          gltfRootObjects['v7'].position.y, 
+          gltfRootObjects['v7'].position.z - (0.70710678118*10)
+        ));
+        startAnimation(gltfRootObjects['v3'], gltfRootObjects['v3'].position.clone(),500,new THREE.Vector3(
+          gltfRootObjects['v3'].position.x - (0.70710678118*10), 
+          gltfRootObjects['v3'].position.y, 
+          gltfRootObjects['v3'].position.z + (0.70710678118*10)
+        ));
+
+        ventmoved = true;
     }else
-    if(object.moved == false){
-    object.position.copy(targetPosition);
+
+    if(object.moved == false & object.name.includes("v") == false){
+    startAnimation(object, object.position.clone(),500,targetPosition);
     }
 }
 
@@ -114,10 +175,15 @@ window.addEventListener('resize', () => {
 //#endregion
 
 //#region materials
+const textureLoader = new THREE.TextureLoader();
+
+const normalMap = textureLoader.load('3DPrint_NormalMap_1K.png');
 const plasticMaterial = new THREE.MeshPhongMaterial({
   color: 0xadd8e6, // Light blue color
-  shininess: 100   // Adjust this for the desired shininess
+  shininess: 100,   // Adjust this for the desired shininess
+  normalMap: normalMap,
 });
+plasticMaterial.normalScale = new THREE.Vector2(1, 1);
 
 const plasticBlackMaterial = new THREE.MeshPhongMaterial({
   color: 0x000000, // black color
@@ -132,14 +198,14 @@ const glassMaterial = new THREE.MeshStandardMaterial({
   metalness: 0.2       // Non-metallic material
 });
 
-const textureLoader = new THREE.TextureLoader();
+
 
 // Adding a background
 let textureEquirec = textureLoader.load( 'bg.webp' );
 textureEquirec.mapping = THREE.EquirectangularReflectionMapping;
 textureEquirec.colorSpace = THREE.SRGBColorSpace;
 
-scene.background = textureEquirec;
+//scene.background = textureEquirec;
 
 const icelMap = textureLoader.load('ice_normal.jpg'); // Replace with your normal map path
 const iceMaterial = new THREE.MeshPhysicalMaterial({
@@ -159,6 +225,7 @@ iceMaterial.normalScale = new THREE.Vector2(1, 0.5); // Adjust x and y scale for
 
 //#region object spawning
 const gltfRootObjects = {};
+let movableObjects = [];
 // Instantiate the loader
 const corps = new GLTFLoader();
 // Load a GLTF resource
@@ -173,6 +240,15 @@ corps.load(
           node.material = plasticMaterial;
           node.name = "corps";
           node.moved = false;
+movableObjects.push(node);
+          node.userData = {
+            animating: false,
+            moved: false,
+            startTime: 0,
+            startPosition: null,
+            endPosition: null,
+            duration: 0
+        };
       }
   });
     scene.add( gltf.scene );
@@ -206,6 +282,7 @@ foot.load(
           node.material = plasticMaterial;
           node.name = "foot";
           node.moved = false;
+movableObjects.push(node);
       }
   });
   gltf.scene.rotation.y = Math.PI/1.55;
@@ -241,6 +318,7 @@ gCloche.load(
           node.material = glassMaterial;
           node.name = "gcloche";
           node.moved = false;
+movableObjects.push(node);
       }
   });
     scene.add( gltf.scene );
@@ -274,6 +352,7 @@ pCloche.load(
           node.material = glassMaterial;
           node.name = "pcloche";
           node.moved = false;
+movableObjects.push(node);
       }
   });
     scene.add( gltf.scene );
@@ -307,6 +386,7 @@ silcRing.load(
           node.material = plasticMaterial;
           node.name = "silcring";
           node.moved = false;
+movableObjects.push(node);
       }
   });
     scene.add( gltf.scene );
@@ -340,6 +420,7 @@ top.load(
           node.material = plasticMaterial;
           node.name = "top";
           node.moved = false;
+movableObjects.push(node);
       }
   });
   
@@ -374,6 +455,7 @@ ice.load(
           node.material = iceMaterial;
           node.name = "ice";
           node.moved = false;
+movableObjects.push(node);
       }
   });
   
@@ -402,6 +484,7 @@ ventil.load(
   
   // Called when the resource is loaded
   function ( gltf ) {
+    movableObjects.push(gltf.scene);
     gltf.scene.traverse(function (node) {
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
@@ -434,6 +517,7 @@ ventil.load(
   function ( gltf ) {
     gltf.scene.traverse(function (node) {
       gltf.scene.rotation.y = Math.PI/4;
+      movableObjects.push(gltf.scene);
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
           node.name = "v2";
@@ -465,6 +549,7 @@ ventil.load(
   function ( gltf ) {
     gltf.scene.traverse(function (node) {
       gltf.scene.rotation.y = Math.PI/2;
+      movableObjects.push(gltf.scene);
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
           node.name = "v3";
@@ -496,10 +581,12 @@ ventil.load(
   function ( gltf ) {
     gltf.scene.traverse(function (node) {
       gltf.scene.rotation.y = 3* Math.PI/4 ;
+      movableObjects.push(gltf.scene);
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
           node.name = "v4";
           node.moved = false;
+          
       }
   });
   
@@ -528,6 +615,7 @@ ventil.load(
   function ( gltf ) {
     gltf.scene.traverse(function (node) {
       gltf.scene.rotation.y = 4* Math.PI/4 ;
+      movableObjects.push(gltf.scene);
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
           node.name = "v5";
@@ -559,6 +647,7 @@ ventil.load(
   function ( gltf ) {
     gltf.scene.traverse(function (node) {
       gltf.scene.rotation.y = 5* Math.PI/4 ;
+      movableObjects.push(gltf.scene);
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
           node.name = "v6";
@@ -590,6 +679,7 @@ ventil.load(
   function ( gltf ) {
     gltf.scene.traverse(function (node) {
       gltf.scene.rotation.y = 6* Math.PI/4 ;
+      movableObjects.push(gltf.scene);
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
           node.name = "v7";
@@ -621,6 +711,7 @@ ventil.load(
   function ( gltf ) {
     gltf.scene.traverse(function (node) {
       gltf.scene.rotation.y = 7* Math.PI/4 ;
+      movableObjects.push(gltf.scene);
       if (node.isMesh) {
           node.material = plasticBlackMaterial;
           node.name = "v8";
@@ -656,14 +747,39 @@ scene.add(light);
 //#endregion
 
 //#region animate
+
+function startAnimation(object, startPosition, duration, endPosition) {
+  object.userData.animating = true;
+  object.userData.startTime = Date.now();
+  object.userData.startPosition = startPosition;
+  object.userData.endPosition = endPosition;
+  object.userData.duration = duration;
+  //console.log("mooving:", object.name);
+}
+
 // Create the animation loop
 const animate = () => {
     // Call animate recursively
     requestAnimationFrame(animate);
+    updateAutoOrbit();
     camera.updateProjectionMatrix();
     // Update the controls
     controls.update();
 
+    const currentTime = Date.now();
+    movableObjects.forEach(child => {
+        if (child.userData.animating) {
+            const elapsedTime = currentTime - child.userData.startTime;
+            const fraction = elapsedTime / child.userData.duration;
+
+            if (fraction < 1) {
+                child.position.lerpVectors(child.userData.startPosition, child.userData.endPosition, fraction);
+            } else {
+                child.position.copy(child.userData.endPosition);
+                child.userData.animating = false;
+            }
+        }
+    });
     // Render the scene
     renderer.render(scene, camera);
 }
